4c36f2aef5dd565490d484df9bd98cf539bcbccb
Hamail Ahmad <hamailahmad@gmail.com>
2025-03-13 20:16:29 +0500

    Initial commit

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,182 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Sample Store App</h3>
+              <p>
+                Developed a Sample Store using Redux Toolkit to deepen my
+                understanding of state management, tackling its complexity while
+                refining my skills in scalable and efficient frontend
+                architecture.
+              </p>
+
+              <div className="flex mt-10 flex-wrap gap-2 mb-4">
+                {["React", "Tailwind CSS", "ReduxToolkit", "Docker"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/hamail143/Sample-Store-using-React"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">TODO App</h3>
+              <p className="text-gray-400 mb-4">
+                A sleek and intuitive Todo App built with React and Tailwind
+                CSS, designed to streamline task management with a clean and
+                modern UI. This project showcases efficient state management,
+                dynamic UI updates, and a fully responsive layout for a seamless
+                user experience.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React JS", "Tailwind CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/hamail143/TODO-App_react"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">
+                Karobar Hub - POS Store App
+              </h3>
+              <p className="text-gray-400 mb-4">
+                Karobar Hub is a POS and Vendor Management mobile app that I
+                developed using Flutter, integrating Firebase for real-time data
+                management and seamless backend operations. This project allowed
+                me to refine my frontend skills by building an intuitive,
+                responsive, and user-friendly interface, enhancing business
+                productivity with efficient vendor tracking and order
+                management.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Dart", "Flutter", "Firebase"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/hamail143/Krobar_hub"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <p className="text-gray-400 mb-4">
+                Scalable chat platform supporting real-time messaging, presence,
+                and group chat features.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Next.JS", "Docker", "PostgressSQL"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
